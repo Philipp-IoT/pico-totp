9470f5453cbec6f508d6711b161cd151d297354f
Philipp Bolte <philipp@bolte.engineer>
2024-10-07 15:14:44 +0000

    Added doxygen comments

--- hd44780/include/hd44780.hpp
+++ hd44780/include/hd44780.hpp
@@ -17,17 +17,47 @@
 namespace Driver::Hd44780
 {
 
+    /**
+     * @brief Driver for HD44780 LCD controller
+     *
+     * @details Currently, only 4 bit mode and 2 line displays are supported.
+     *          It is more like a proof of concept than a full-fledged driver.
+     */
     class Hd44780
     {
     public:
+        /**
+         * @brief The controller is configured inside the constructor
+         *
+         * @param pio PIO to use
+         * @param sm  State machine to use
+         * @param pin First pin. See hd44780.pio for the pin configuration.
+         */
         Hd44780(PIO pio, uint8_t sm, uint8_t pin);
 
+        /**
+         * @brief Disables the display and unclaims the state machine
+         */
         virtual ~Hd44780();
 
+        /**
+         * @brief Clears the display
+         */
         void ClearDisplay();
 
+        /**
+         * @brief Sets the cursor to the given position
+         *
+         * @param row Row (0, 1)
+         * @param col Column (0-15)
+         */
         void SetCursor(uint8_t row, uint8_t col);
 
+        /**
+         * @brief Writes the given text to the display
+         *
+         * @param text Text to write
+         */
         void Write(std::string_view text);
 
     private:
