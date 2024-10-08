304dca3799a7e60571728d7f3d73994e7293bf5b
Philipp Bolte <philipp@bolte.engineer>
2024-10-08 07:30:45 +0000

    Add interface for generic RTC

---
+++ interface_rtc/include/interface_rtc.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file 
+ * @date 2024-10-08
+ * @author Philipp Bolte (philipp@bolte.engineer)
+ * 
+ * @brief Generic interface for a real-time clock using callbacks
+ */
+
+#pragma once
+
+class InterfaceRTC {
+public:
+    virtual ~InterfaceRTC() = default;
+
+    // Set the time using a UNIX timestamp
+    virtual bool setTime(int64_t unixTimestamp) = 0;
+
+    // Read the current time as a UNIX timestamp
+    virtual bool readTime(std::function<void(int64_t)> onSuccess) const = 0;
+};
